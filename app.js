d6a30cea934ac1efb51c6330ab2e18aace4eae37
sohahib <syaoda@yahoo.fr>
2023-06-15 18:38:40 -0400

    removing click event after first click and after a match as well

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ const images = ()=>[
 
 const placeholder = {imageSrc:'./images/question-mark.svg', name:'question-mark'};
 
+
+
 const randomImage = ()=>{
     const data = images()
     data.sort(()=> Math.random() - 0.5);
@@ -45,38 +47,15 @@ let firstImageNname = ''
 let totalScore = 0
 let firstCard 
 
-
 function loadImages() {
     const data = randomImage()
     data.forEach(item => {
         game.innerHTML += `<div class="uno-block">
-        <img class="placeholder " src="${placeholder.imageSrc}" alt="${placeholder.name}">
-        <img class="mistery" src="${item.imageSrc}" name="${item.name}" alt="${item.name}">
+        <img class="placeholder  " src="${placeholder.imageSrc}" alt="${placeholder.name}">
+        <img class="mistery " src="${item.imageSrc}" name="${item.name}" alt="${item.name}">
         </div>`
     });  
-    const showMystery = (element) => {
-        element.classList.add('reverse')
-
-         if(isFirst == 0){
-            isFirst = 1
-            firstImageNname = element.children[1].name
-            firstCard = element
-         }else if (isFirst == 1 ){
-            isFirst = 0
-            if(firstImageNname == element.children[1].name){
-                totalScore++
-                score.innerHTML =`Score: ${totalScore}`
-                console.log('yay!')
-            }else{
-                console.log('not a match')
-                setTimeout(() => {
-                    element.classList.remove('reverse')
-                    firstCard.classList.remove('reverse')
-                }, 1000);
-            }
-         }
-
-    }
+   
     
     setTimeout(() => {
         document.querySelectorAll('.uno-block').forEach(x => {
@@ -86,11 +65,37 @@ function loadImages() {
         })
     }, 1000);
 
-
-
+    
 }
 loadImages()
 
+const showMystery = (element) => {
+    element.classList.add('reverse')
+
+     if(isFirst == 0){
+        isFirst = 1
+        firstImageNname = element.children[1].name
+        firstCard = element
+        firstCard.classList.add('unclickable')
+     }else if (isFirst == 1 ){
+        isFirst = 0
+        if(firstImageNname == element.children[1].name){
+            totalScore++
+            score.innerHTML =`Score: ${totalScore}`
+            console.log('yay!')
+            element.classList.add('unclickable')
+        }else{
+            console.log('not a match')
+            setTimeout(() => {
+                element.classList.remove('reverse')
+                firstCard.classList.remove('reverse')
+            }, 1000);
+            firstCard.classList.remove('unclickable')
+            element.classList.remove('unclickable')
+        }
+     }
+
+}
 
 
 
